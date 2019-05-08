aa952b69e471a52e3678e647fd8086201ea11754
MrNoOne007 <ramicmujo@hotmail.com>
2019-05-08 13:59:12 +0200

    Dev branch initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,9 +15,10 @@ describe('AppComponent', () => {
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.debugElement.componentInstance;
+  expect(app).toBeTruthy();
   });
 
   it(`should have as title 'my-app'`, () => {
@@ -26,10 +27,13 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('my-app');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-app!');
-  });
+  newFunction();
+  function newFunction() {
+    it('should render title in a h1 tag', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-app!');
+    });
+  }
 });
